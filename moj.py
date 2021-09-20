95ff0cbf349f919975f55114bf4d8561aeb45929
Harsh <hnavadiya1802@gmail.com>
2021-09-20 10:34:08 +0530

    added args

--- moj.py
+++ moj.py
@@ -5,29 +5,12 @@ import sys
 
 
 
-#Taxonomy on 01 July 2021
-# Do not set U and P  as shortcuts.
-# Comedy / Romance / Motivation / Attitude / Sadness / Anger / Kindness	Romance & Heartbreak	Romantic Songs	Couple (BF + GF) Romance
-# Comedy / Romance / Motivation / Attitude / Sadness / Anger / Kindness	Romance & Heartbreak	Romantic Songs	Husband Wife Romance
-# Comedy / Romance / Motivation / Attitude / Sadness / Anger / Kindness	Romance & Heartbreak	Romantic Songs	One Sided Love
-# Comedy / Romance / Motivation / Attitude / Sadness / Anger / Kindness	Romance & Heartbreak	Romantic Songs	Break up & Betrayal 
 
 
-
-def key(a,b,c,d,e,f):
-	keyboard.press(a) #It will select No 
-	time.sleep(0.1)
-	keyboard.press(b) #L0
-	time.sleep(0.1)
-	keyboard.press(c) #L1
-	time.sleep(0.1)
-	keyboard.press(d) #L2
-	time.sleep(0.1)
-	keyboard.press(e) #L3
-	time.sleep(0.1)
-	keyboard.press(f) #L4
-	time.sleep(0.1)
-	
+def key(*args):
+	for i in args:
+		keyboard.press(i)
+		time.sleep(0.1)
 
 
 
@@ -35,22 +18,22 @@ def key(a,b,c,d,e,f):
 def check(a):
 	if keyboard.is_pressed(a):
 		if a == 'q':
-			key('1','2','2','3','2','-') #Rom_song_gf_bf
+			key('1','2','2','3','2') #Rom_song_gf_bf
 			check(keyboard.read_key())
 			
 
 		if a == 'w':
-			key('1','2','2','3','3','-') #rom_song_husband_wife
+			key('1','2','2','3','3') #rom_song_husband_wife
 			check(keyboard.read_key())
 			
 
 		if a == 'e':
-			key('1','2','2','3','4','-') #rom_song_one_side
+			key('1','2','2','3','4') #rom_song_one_side
 			check(keyboard.read_key())
 			
 
 		if a == 'r':
-			key('1','2','2','3','5','-') #rom_song_brkup
+			key('1','2','2','3','5') #rom_song_brkup
 			check(keyboard.read_key())
 			
 
@@ -67,70 +50,70 @@ def check(a):
 			check(keyboard.read_key())
 			
 		if a == 'm':
-			key('1','8','1','1','1','-') #mother
+			key('1','8','1','1','1') #mother
 			check(keyboard.read_key())
 			
 
 
 		if a == 'l':
-			key('1','2','1','5','6','-') #Kids_humour
+			key('1','2','1','5','6') #Kids_humour
 			check(keyboard.read_key())
 			
 
 		if a == 'f':
-			key('1','2','1','5','8','-') #other_funny_act
+			key('1','2','1','5','8') #other_funny_act
 			check(keyboard.read_key())
 			
 
 
 		if a == 't':
-			key('1','4','4','1','6','-') #travel
+			key('1','4','4','1','6') #travel
 			check(keyboard.read_key())
 			
 
 		if a == 'c':
-			key('1','4','8','2','3','-') #cow_ox_buffalo
+			key('1','4','8','2','3') #cow_ox_buffalo
 			check(keyboard.read_key())
 			
 
 		if a == 's':
-			key('1','4','8','2','6','-') #other_street_animal
+			key('1','4','8','2','6') #other_street_animal
 			check(keyboard.read_key())
 			
 
 
 		if a == 'd':
-			key('1','5','2','1','-','-') #Devotion_name_choice
+			key('1','5','2','1') #Devotion_name_choice
 			check(keyboard.read_key())
 			
 
 		if a == 'o':
-			key('1','5','2','1','0','-') #other_devotional
+			key('1','5','2','1','0') #other_devotional
 			check(keyboard.read_key())
 			
 
 		if a == 'h':
-			key('1','5','2','2','0','-') #hindu_place of worship
+			key('1','5','2','2','0') #hindu_place of worship
 			check(keyboard.read_key())
 			
 
 		if a == 'b':
-			key('1','2','4','2','2','-') #boy_attitude_actr
+			key('1','2','4','2','2') #boy_attitude_actr
 			check(keyboard.read_key())
 			
 
 		if a == 'n':
-			key('1','2','4','3','2','-') #attitude_actr
+			key('1','2','4','3','2') #attitude_actr
 			check(keyboard.read_key())
 			
 
 		if a == 'y':
-			key('1','2','2','2','-','-') #rom_act
+			key('1','2','2','2') #rom_act
 			check(keyboard.read_key())
 			
 
 		if a == '+':
-			key('1','2','2','4','-','-') #other_rom
+			key('1','2','2','4') #other_rom
 			check(keyboard.read_key())
 			
 
@@ -140,13 +123,6 @@ def check(a):
 			time.sleep(15)
 			check(keyboard.read_key())
 			
-			
-			
-
-	
-
-
-
 
 
 while True:
@@ -155,8 +131,3 @@ while True:
 	check(keyboard.read_key())
 	
 	
-
-
-
-
-
